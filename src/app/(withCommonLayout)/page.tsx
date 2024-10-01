54eb655028f9ee17e3a1301add99d2a75c7b3ffa
irfanurislam <mdirfanurislam@gmail.com>
2024-10-01 15:30:19 +0600

    add section: toprated doctors, whyus

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -2,12 +2,16 @@ import HeroSection from "@/components/UI/HomePage/HeroSection/HeroSection";
 import Specialist from "@/components/UI/HomePage/Specialist/Specialist";
 import { Button } from "@mui/material";
 import React from "react";
+import TopRatedDoctors from "../../components/UI/HomePage/TopRatedDoctors/TopRatedDoctors";
+import WhyUs from "../../components/UI/HomePage/WhyUs/WhyUs";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection></HeroSection>
       <Specialist />
+      <TopRatedDoctors />
+      <WhyUs />
     </>
   );
 };
