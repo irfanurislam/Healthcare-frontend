9d542480920d23e328fcdd1adb9594049cc815e8
irfanurislam <mdirfanurislam@gmail.com>
2024-10-06 16:49:02 +0600

    add : register data page:

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import { Container, Stack } from "@mui/material";
+import React from "react";
 
 const LoginPage = () => {
-    return (
-        <div>
-            <h1>login page</h1>
-        </div>
-    );
+  return (
+    <Container>
+      <Stack
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      ></Stack>
+    </Container>
+  );
 };
 
 export default LoginPage;
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,10 +1,140 @@
+import {
+  Stack,
+  Container,
+  Box,
+  Typography,
+  TextField,
+  Grid,
+  Button,
+} from "@mui/material";
+import Image from "next/image";
+import assets from "@/assets";
 import React from "react";
+import Link from "next/link";
+import { useForm, SubmitHandler } from "react-hook-form";
 
+type Inputs = {
+  example: string;
+  exampleRequired: string;
+};
 const RegisterPage = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
   return (
-    <div>
-      <h1>register pages</h1>
-    </div>
+    <Container>
+      <Stack
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 600,
+            width: "100%",
+            boxShadow: 1,
+            borderRadius: 1,
+            p: 4,
+
+            textAlign: "center",
+          }}
+        >
+          <Stack
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Box>
+              <Image
+                src={assets?.svgs.logo}
+                width={50}
+                height={50}
+                alt="logo"
+              />
+            </Box>
+            <Box>
+              <Typography variant="h6" fontWeight={600}>
+                Patience Register
+              </Typography>
+            </Box>
+          </Stack>
+          <Box>
+            <form>
+              <Grid Container spacing={2}>
+                <Grid item md={12}>
+                  <TextField
+                    id="Name"
+                    label="Outlined"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    id="Email"
+                    type="email"
+                    label="Outlined"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    id="Password"
+                    type="password"
+                    label="Outlined"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    id="Contact Number"
+                    type="tel"
+                    label="Outlined"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    id="Address"
+                    type="text"
+                    label="Outlined"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                sx={{
+                  margin: "10px 0px ",
+                }}
+                fullWidth={true}
+              >
+                Register
+              </Button>
+              <Typography component="h6" fontWeight={600}>
+                Do You Already Have an account ?{" "}
+                <Link href="/login">login</Link>
+              </Typography>
+            </form>
+          </Box>
+        </Box>
+      </Stack>
+    </Container>
   );
 };
 
